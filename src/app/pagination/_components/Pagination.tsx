6c9e4314e8e8f837b7e7e7cdd4257c6d51deee6d
hyeonjun-L <beeminor60@gmail.com>
2024-07-15 05:57:39 +0900

    feat: pagination 디자인 수정

--- src/app/pagination/_components/Pagination.tsx
+++ src/app/pagination/_components/Pagination.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import PaginationController from './PaginationController';
 import { Photo } from '@/types/image';
+import Link from 'next/link';
 
 interface PaginationProps {
   images: Photo[];
@@ -12,18 +13,27 @@ const Pagination = ({ images, currentPage }: PaginationProps) => {
 
   return (
     <main className="mt-14">
-      <ul className="grid gap-4 px-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <ul className="grid gap-4 px-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 ">
         {images.map(({ id, urls, alternative_slugs }, index) => (
           <li
             key={id + index}
-            className="animate-fadeInUp relative aspect-square"
+            className="relative aspect-square animate-fadeInUp shadow-[8px_8px_16px_#5a5a5a,-8px_-8px_16px_#ffffff]"
           >
-            <Image
-              src={urls.small_s3}
-              alt={alternative_slugs.ko}
-              fill
-              sizes="(max-width: 640px) 90vw, (max-width: 768px) 50vw, (max-width: 1280px) 33vw, 25vw"
-            />
+            <Link
+              href={`/photos?image=${urls.regular}`}
+              scroll={false}
+              className="group"
+            >
+              <div className="absolute left-0 top-0 z-10 hidden size-full backdrop-brightness-75 group-hover:block" />
+              <Image
+                src={urls.small_s3}
+                alt={alternative_slugs.ko}
+                fill
+                sizes="(max-width: 640px) 90vw, (max-width: 768px) 50vw, (max-width: 1280px) 33vw, 25vw"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOsa2yqBwAFCAICLICSyQAAAABJRU5ErkJggg=="
+              />
+            </Link>
           </li>
         ))}
       </ul>
